f62b6c1a401340ec39a22edf3f3eeefbcafb1b14
Craig Dupuis <craigdupuis@Craig-Dupuis-MacBook-Pro.local>
2018-01-28 15:38:15 -0600

    organizes folders and adds random number to target score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,35 @@
-<script>
-function getRndInteger(min, max) {
-   return Math.floor(Math.random() * (120 - 19)) + 19;
-}
-</script>
+var targetScore = function () {
+  return Math.floor(Math.random() * (120 - 19)) + 19;
+  //push to dom
+};
+$("#targetScore").text(targetScore);
+targetScore();
 
 
 
-//JS gone over in class
-var rand = [4,5,7,8];
+// var gameStart = function () {
 
-var clickAction = function(){
-  var index = $(this).attr("data-num")
-  console.log("data", index);
-  console.log("random", rand[index]);
-}
+// };
 
-$("button").on("click", clickAction);
+// gameStart();
+
+
+// //Option for random number genrator
+// <script>
+//   function getRndInteger(min, max) {
+//    return Math.floor(Math.random() * (120 - 19)) + 19;
+// }
+// </script>
+
+
+
+// //JS gone over in class
+// var rand = [4, 5, 7, 8];
+
+// var clickAction = function () {
+//   var index = $(this).attr("data-num")
+//   console.log("data", index);
+//   console.log("random", rand[index]);
+// }
+
+// $("button").on("click", clickAction);
