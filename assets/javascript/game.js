01349b5af97d0827e17917525a4b589e75f2af7a
Craig Dupuis <craigdupuis@Craig-Dupuis-MacBook-Pro.local>
2018-01-28 15:57:52 -0600

    adds random numbers to crystal buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,35 @@
-var targetScore = function () {
-  return Math.floor(Math.random() * (120 - 19)) + 19;
-  //push to dom
-};
+//Random number for target score
+var targetScore = Math.floor(Math.random() * (120 - 19)) + 19;
 $("#targetScore").text(targetScore);
-targetScore();
+console.log("Target score is ", targetScore);
+
+//Crystal button values
+var crystalButton1 = Math.floor(Math.random() * (12 - 1)) + 1;
+// $("#crystalButton1").text(crystalButton1);
+// $("#crystalButton1").on("click", updateScore); - to be used later
+// $("#crystalButton1").click(crystalButton1); - creates an error
+console.log("Button 1 is ", crystalButton1);
+
+var crystalButton2 = Math.floor(Math.random() * (12 - 1)) + 1;
+console.log("Button 2 is ", crystalButton2);
+
+var crystalButton3 = Math.floor(Math.random() * (12 - 1)) + 1;
+console.log("Button 3 is ", crystalButton3);
+
+var crystalButton4 = Math.floor(Math.random() * (12 - 1)) + 1;
+console.log("Button 4 is ", crystalButton4);
+
+
+//User's score
+// var userScore = 0
+
 
 
 
 // var gameStart = function () {
+
+//   // $("#crystalButton1").click(updateScore); - when user clicks the crystal it adds to userScore
+
 
 // };
 
@@ -23,7 +45,7 @@ targetScore();
 
 
 
-// //JS gone over in class
+// JS gone over in class
 // var rand = [4, 5, 7, 8];
 
 // var clickAction = function () {
